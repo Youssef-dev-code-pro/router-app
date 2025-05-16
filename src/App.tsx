62a9e98f75199659bed7cbe8edd2215f349fb45c
Youssef-dev-code-pro <youssefehabtatake@gmail.com>
2025-05-16 19:32:03 +0300

    إضافة الملفات

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
